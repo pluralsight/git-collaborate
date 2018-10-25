a68130b1af8420ef6496683c8fcfb8cd86ee4285
Parker Holladay <parker.holladay@gmail.com>
2018-10-25 16:51:11 -0600

    Add rsa key input/selection to client

--- src/client/menu/users/components/user-form/index.js
+++ src/client/menu/users/components/user-form/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
+import { remote } from 'electron'
 import { bool, func, shape, string } from 'prop-types'
 
 import Button from '../../../components/button'
@@ -8,7 +9,7 @@ import css from './index.css'
 const userType = shape({
   name: string.isRequired,
   email: string.isRequired,
-  rsaKeyPath: string.isRequired,
+  rsaKeyPath: string,
   active: bool
 })
 
@@ -20,6 +21,13 @@ export default class UserForm extends React.Component {
     onChange: func.isRequired,
     onClose: func.isRequired,
     confirmLabel: string.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSelectingRsaKey: false
+    }
   }
 
   componentDidUpdate(lastProps) {
@@ -34,7 +42,7 @@ export default class UserForm extends React.Component {
 
   isValid = () => {
     const { user } = this.props
-    return user && user.name && user.email
+    return user && user.name && user.email && !this.state.isSelectingRsaKey
   }
 
   handleFieldChange = e => {
@@ -42,6 +50,19 @@ export default class UserForm extends React.Component {
     this.props.onChange({
       ...this.props.user,
       [id]: value
+    })
+  }
+  handleAddRsaKey = () => {
+    this.setState({ isSelectingRsaKey: true })
+    remote.dialog.showOpenDialog({
+      properties: ['openFile', 'showHiddenFiles', 'treatPackageAsDirectory']
+    }, this.handleRsaKeySelected)
+  }
+  handleRsaKeySelected = paths => {
+    this.setState({ isSelectingRsaKey: false })
+    this.props.onChange({
+      ...this.props.user,
+      rsaKeyPath: (paths && paths[0]) || this.props.user.rsaKeyPath
     })
   }
 
@@ -65,6 +86,20 @@ export default class UserForm extends React.Component {
             value={user.email}
             placeholder="Email"
             onChange={this.handleFieldChange} />
+          <div styleName="rsa-field-container">
+            <input
+              id="rsaKeyPath"
+              styleName="rsa-field"
+              value={user.rsaKeyPath}
+              placeholder="Path to RSA key"
+              onChange={this.handleFieldChange} />
+            <Button
+              styleName="browse-button"
+              onClick={this.handleAddRsaKey}
+              disabled={this.state.isSelectingRsaKey}>
+              Browse
+            </Button>
+          </div>
         </div>
         <div styleName="button-section">
           <Button type={Button.types.confirm} onClick={onConfirm} disabled={!this.isValid()}>{confirmLabel}</Button>
