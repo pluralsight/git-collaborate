5e44f6dc6c1eb197cbe1b3ccab4774598772a9ff
Parker Holladay <parker-holladay@pluralsight.com>
2020-04-05 05:15:32 -0600

    Add cli command to show/focus client

---
+++ src/cli/commands/show.js
@@ -0,0 +1,18 @@
+import { getMenubar } from '../utils'
+
+export const command = 'show'
+export const describe = 'Show and focus running git-switch client'
+
+export const builder = yargs =>
+  yargs
+    .usage('Usage:\n  git-switch show')
+    .version(false)
+
+export const handler = args => {
+  const { doWork } = args
+
+  // show window of first instance
+  if (!doWork) {
+    getMenubar().showWindow()
+  }
+}
